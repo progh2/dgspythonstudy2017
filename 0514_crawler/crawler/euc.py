c1218c2ebd6918d648d5af235bd5bd3773718d70
Gihun Ham <progh2@gmail.com>
2017-05-14 15:43:31 +0900

    크롤러 소스 개선 및 주석 추가

--- 0514_crawler/crawler/euc.py
+++ 0514_crawler/crawler/euc.py
@@ -1,10 +1,11 @@
-# parser.py
+# euc.py
+# euc-kr로 된 사이트 가져오는 예제
 from urllib.request import urlopen
-
 from requests.packages.idna import unicode
 
-url = 'http://mba.yonsei.ac.kr/recruit.asp?mid=m04_05'
+
+url = 'http://mba.yonsei.ac.kr/recruit.asp?mid=m04_05' # 가져오고자 하는 보기드문 euc-kr로 된 사이트
 u = urlopen(url)
-s = unicode(u.read(), "euc-kr")
+s = unicode(u.read(), "euc-kr") # 두번째 인자로 원하는 인자를 지정할 수 있음.
 print(s)
 u.close()
--- 0514_crawler/crawler/main.py
+++ 0514_crawler/crawler/main.py
@@ -4,14 +4,20 @@ from bs4 import BeautifulSoup as bs
 import json
 import os
 
+
 # python파일의 위치
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # HTTP GET Request
 s = rst.Session()
-req =  s.get('http://reading.ssem.or.kr/r/reading/search/schoolCodeSetting.jsp?schoolCode=24001', verify=False)
-req =  s.get('http://reading.ssem.or.kr/r/reading/search/schoolSearchForm.jsp', verify=False)
-req =  s.post('http://reading.ssem.or.kr/r/reading/search/schoolSearchResult.jsp?searchPageName=schoolSearchForm&division1=ALL&searchCon1=JAVA&connect1=A&division2=TITL&searchCon2=&connect2=A&division3=PUBL&searchCon3=&dataType=ALL', verify=False)
+
+# 이 사이트는 첫 페이지에 접속해서 쿠키로 학교 코드 값을 저장하고 진행해야 데이터 값을 가져올 수 있다.
+url_index='http://reading.ssem.or.kr/r/reading/search/schoolCodeSetting.jsp?schoolCode=24001'
+req = s.get(url_index, verify=False)
+
+url_searchresult='http://reading.ssem.or.kr/r/reading/search/schoolSearchResult.jsp'
+search_word = '정석'  # 책 검색어(모든 필드 검색)
+req = s.post(url_searchresult + '?searchPageName=schoolSearchForm&division1=ALL&searchCon1=%s&dataType=ALL' % search_word, verify=False)
 
 # HTML 소스 가져오기
 html = req.text
@@ -40,10 +46,11 @@ for book in books:
     tds = book.select('td')
     # Tag안의 텍스트
     print(tds[0].find('a').get('onclick'))
-    #print( tds[0].find('a span').text)
+    print("제목:" + tds[0].find('a').text)
     print("저자:"+tds[1].text.strip())
     print("출판사:"+tds[2].text.strip().replace("\t", "").replace("\n(", "("))
     print("코드:"+tds[3].text.strip())
+    print("")
     # Tag의 속성을 가져오기(ex: href속성)
     # print(p.get('src'))
 #
